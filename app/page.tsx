dd8dbbaba0ce1f2b3a45e00a48ebe280d4d8e800
Karina Tulegenova <241438@astanait.edu.kz>
2025-08-17 16:09:18 +0500

    Фильрация

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from '@/components/shared';
+import { Container, Title, Filters } from '@/components/shared';
 import { TopBar } from '@/components/shared/top-bar';
 
 export default function Page() {
@@ -6,7 +6,23 @@ export default function Page() {
 		<Container className='mt-10'>
 			<Title text="Все пиццы" size="lg" className='font-extrabold' />
 		</Container>
+
 		<TopBar />
-		<div style={{ height: '3000px' }}></div>
+
+		<Container className='mt-10 pb-14'>
+			<div className='flex gap-[60px]'>
+				{/*Фильрация*/}
+				<div className='w-[250px]'>
+					<Filters />
+				</div>
+
+				{/*Список товаров*/}
+				<div className='flex-1'>
+					<div className='flex flex-col gap-16'>
+						Список товаров
+					</div>
+				</div>
+			</div>
+		</Container>
 	</>;
 }
